45c1eb82f5d3781d8ac71ec08ceed7caa9d1c615
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-03-02 19:09:39 +0530

    Update

--- app.js
+++ app.js
@@ -7,19 +7,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user");
 
+app.use(cors());
 
 const mongoUsername = encodeURIComponent(process.env.MONGO_USER);
 const mongoPassword = encodeURIComponent(process.env.MONGO_PASS);
 const mongocluster = process.env.MONGO_CLUSTER;
 const mongoAuthSource = process.env.MONGO_AUTH_SOURCE;
 const mongoAuthMechanism = process.env.MONGO_AUTH_MECHANISM;
+
+
 let uri = `mongodb+srv://${mongoUsername}:${mongoPassword}@openchat.zvgfv36.mongodb.net/?retryWrites=true&w=majority&appName=openchat`;
 mongoose.connect(uri || 'mongodb://localhost:27017/sample-db',);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
 
-app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
 
