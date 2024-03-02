6c61cd22d4b9967c03f7c2cd4dffbc5bb4421541
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-03-02 17:03:40 +0530

    Update

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/user");
 
-// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sample-db',);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
 
-// Apply CORS middleware
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-// Define routes
 app.use('/api/user', userRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
